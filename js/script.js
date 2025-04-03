7c963d04c5dbf8af1bb7aeceeaa9ede50c19e34f
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-03 15:10:32 +0200

    [UPD] Generate email function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 //Recupero elementi dal DOM
 const emailList = document.getElementById('email-list')
 
-//AJAX Request
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) =>{
-    console.log(resp.data.response)
-})
+//Generate email address Function
+const generateEmail = (num) =>{
+    //Empty array
+    let genEmails = []
+    //Empty string
+    let string = ''
+    //FOR Loop
+    for(let i = 0; i < num; i++){
+        //AJAX Request
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) =>{
+            //Pushing email address in array
+            genEmails.push(resp.data.response)
+            //Check if emails address generated are 10
+            if (genEmails.length === num){
+                //generating string
+                string = genEmails.map(email => `<li>${email}</li>`).join('');
+                //inner.HTML the string
+                emailList.innerHTML = string
+            }
+        })
+    }
+}
